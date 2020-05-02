677bb18d83353ea4bd00f250a94da29f693c6b0a
bumis95 <bumis95@gmail.com>
2020-05-02 20:03:04 +0300

    add Combinator

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,15 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        // c = n! / (n-m)! * m!
+        val c = array[0]
+        val n = array[1]
+        if(n <= 0) return null
+        for(m in 0..n)
+            if(c * factorial(n-m) * factorial(m) == factorial(n)) return m
+        return null
     }
+
+    fun factorial(n: Int) = (2..n).fold(1L, Long::times)
 }
