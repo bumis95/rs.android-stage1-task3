3a5e5c2e06c39696ed50e52323565365143dbfce
bumis95 <bumis95@gmail.com>
2020-05-02 20:08:33 +0300

    update TelephoneFinder

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -6,30 +6,19 @@ class TelephoneFinder {
 
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
 
-        val one = listOf('2','4')
-        val two = listOf('1','3','5')
-        val three = listOf('2','6')
-        val four = listOf('1','5','7')
-        val five = listOf('2','4','6','8')
-        val six = listOf('3','5','9')
-        val seven = listOf('4','8')
-        val eight = listOf('5','7','9','0')
-        val nine = listOf('6','8')
-        val zero = listOf('8')
-
         if(number.matches("[0-9]*".toRegex())) {
             number.forEachIndexed { index, char ->
                 when(char) {
-                    '0' -> replaceChars(index, number, zero)
-                    '1' -> replaceChars(index, number, one)
-                    '2' -> replaceChars(index, number, two)
-                    '3' -> replaceChars(index, number, three)
-                    '4' -> replaceChars(index, number, four)
-                    '5' -> replaceChars(index, number, five)
-                    '6' -> replaceChars(index, number, six)
-                    '7' -> replaceChars(index, number, seven)
-                    '8' -> replaceChars(index, number, eight)
-                    else -> replaceChars(index, number, nine)
+                    '0' -> replaceChars(index, number, listOf('8'))
+                    '1' -> replaceChars(index, number, listOf('2','4'))
+                    '2' -> replaceChars(index, number, listOf('1','3','5'))
+                    '3' -> replaceChars(index, number, listOf('2','6'))
+                    '4' -> replaceChars(index, number, listOf('1','5','7'))
+                    '5' -> replaceChars(index, number, listOf('2','4','6','8'))
+                    '6' -> replaceChars(index, number, listOf('3','5','9'))
+                    '7' -> replaceChars(index, number, listOf('4','8'))
+                    '8' -> replaceChars(index, number, listOf('5','7','9','0'))
+                    else -> replaceChars(index, number, listOf('6','8'))
                 }
             }
         }
