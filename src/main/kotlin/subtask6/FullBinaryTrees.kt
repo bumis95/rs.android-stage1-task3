609add82dba5b5fb1f42e71b01b75e93689d4161
bumis95 <bumis95@gmail.com>
2020-05-05 18:07:29 +0300

    add BinaryTree

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,54 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+
+        val arrayOfNodes = getNodes(count)
+        val result = StringBuilder()
+
+        arrayOfNodes?.map { el ->
+            val node = "[0,$el]"
+            val nodeWithoutNull = node.replace("(null,)+]".toRegex(), "]")
+            result.append(nodeWithoutNull).deleteCharAt(result.length - 2)
+        }
+        println("[${result}]")
+        return "[${result}]"
+    }
+
+    class Node(var root: Int = 0) {
+
+        var left: Node? = null
+        var right: Node? = null
+
+        override fun toString(): String {
+            return if (left == null && right == null)
+                "${left},${right},"
+            else
+                "${left?.root},${right?.root},$left$right"
+        }
+    }
+
+    fun getNodes(n: Int): List<Node>? {
+
+        val list = mutableListOf<Node>()
+        var i = 1
+
+        if (n % 2 == 0) return list
+        if (n == 1) {
+            list.add(Node())
+            return list
+        }
+
+        while (i < n) {
+            for (left in getNodes(i)!!)
+                for (right in getNodes(n - i - 1)!!) {
+                    val root = Node()
+                    root.left = left
+                    root.right = right
+                    list.add(root)
+                }
+            i += 2
+        }
+        return list
     }
 }
